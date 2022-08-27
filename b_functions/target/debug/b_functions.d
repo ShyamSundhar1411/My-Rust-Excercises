0e5f6858926caa8fcab0b0ff44f32a2f06b35828
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-08-27 15:09:36 +0530

    Simple Tyles and Module System

---
+++ b_functions/target/debug/b_functions.d
@@ -0,0 +1,1 @@
+D:\Projects\Rust\exercise\b_functions\target\debug\b_functions.exe: D:\Projects\Rust\exercise\b_functions\src\main.rs
---
+++ b_functions/target/debug/deps/b_functions.d
@@ -0,0 +1,5 @@
+D:\Projects\Rust\exercise\b_functions\target\debug\deps\b_functions.exe: src\main.rs
+
+D:\Projects\Rust\exercise\b_functions\target\debug\deps\b_functions.d: src\main.rs
+
+src\main.rs:
