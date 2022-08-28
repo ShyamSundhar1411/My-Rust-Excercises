024fac2570d1ff561d68276394e162ed85ab5bc0
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-08-28 23:54:51 +0530

    Strings and Control Types

--- d_control_flow_strings/src/main.rs
+++ d_control_flow_strings/src/main.rs
@@ -14,6 +14,15 @@ fn main() {
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
+        if arg=="sum"{
+            sum();
+        }
+        else if arg == "double"{
+            double();
+        }
+        else{
+            count(arg);
+        }
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
@@ -28,6 +37,9 @@ fn main() {
 
 fn sum() {
     let mut sum = 0;
+    for i in 7..=23{
+        sum+=i;
+    }
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
@@ -42,8 +54,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
-
+    while x<500{
+        count+=1;
+        x = x*2;
+    }
     println!("You can double x {} times until x is larger than 500", count);
 }
 
@@ -52,7 +66,40 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
-
-    println!(); // This will output just a newline at the end for cleanliness.
+    'outer:loop{
+        loop{
+            loop{
+                loop{
+                    loop{
+                        loop{
+                            loop{
+                                loop{
+                                    println!("{}",arg);
+                                break;
+                                }
+                                println!("{}",arg);
+                                break;
+                            }
+                            println!("{}",arg);
+                            break;
+                        }
+                        println!("{}",arg);
+                        break;
+                    }
+                    println!("{}",arg);
+                    break;
+                }
+                println!("{}",arg);
+                break;
+            }
+            println!("{}",arg);
+            break;
+        }
+        println!("{}",arg);
+        break;
+    }
+            
+            
+    println!();
+     // This will output just a newline at the end for cleanliness.
 }
