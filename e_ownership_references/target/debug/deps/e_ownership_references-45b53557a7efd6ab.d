8f7357ce0a0f6d8b7e406d7d77e7a55e4056a0ad
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-08-31 14:55:33 +0530

    Ownerships

--- e_ownership_references/target/debug/deps/e_ownership_references-45b53557a7efd6ab.d
+++
@@ -1,5 +0,0 @@
-d:\Projects\Rust\exercise\e_ownership_references\target\debug\deps\e_ownership_references-45b53557a7efd6ab.rmeta: src\main.rs
-
-d:\Projects\Rust\exercise\e_ownership_references\target\debug\deps\e_ownership_references-45b53557a7efd6ab.d: src\main.rs
-
-src\main.rs:
